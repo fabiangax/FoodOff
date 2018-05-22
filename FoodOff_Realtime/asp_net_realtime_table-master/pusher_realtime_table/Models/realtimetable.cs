f02a5e54d36ecdf7e8c14969331048369feacbf6
Fabian Gaxiola <fabiangax@gmail.com>
2018-05-21 22:27:07 -0700

    Almos Final Version
    We need to complete the anothers foodtrucks and also assign all order menus.

---
+++ FoodOff_Realtime/asp_net_realtime_table-master/pusher_realtime_table/Models/realtimetable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace pusher_realtime_table.Models
+{
+    public class RealtimeTable
+    {
+        [Key]
+        public int id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string title { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Nombre_Foodtruck { get; set;  }
+        [Required]
+        public int year { get; set; }
+        [Required]
+        [MaxLength(2000)]
+        public string Desc_Orden { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public String Metodo_Pago { get; set; }
+    }
+    public enum payments {
+        Debito,
+        Credito,
+        Efectivo
+    }
+}
